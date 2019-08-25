e9c42c736b7c05029dc3986680f8cd74435de631
ml250271 <lukicmilica@hotmail.com>
2019-08-25 22:18:35 +0200

    add react-leaflet

--- src/Grid.js
+++ src/Grid.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import GridItem from "./GridItem";
+import OfficesMap from "./OfficesMap";
 
 const GridItemWrapper = styled.div`
     display: grid;
--- src/OfficesMap.js
+++ src/OfficesMap.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 
-const OfficesMap = () => {
-    return <h3>OfficesMap</h3>;
-};
+class OfficesMap extends React.Component {
+    componentDidMount() {}
+    render() {
+        //const position = [51.505, -0.09];
+        return null;
+        //(
+        //     <div>
+        //         {" "}
+        //         <Map center={position} zoom={13}>
+        //             <TileLayer
+        //                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        //                 url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
+        //             />
+        //             <Marker position={position}>
+        //                 <Popup>
+        //                     A pretty CSS3 popup.
+        //                     <br />
+        //                     Easily customizable.
+        //                 </Popup>
+        //             </Marker>
+        //         </Map>
+        //         );
+        //     </div>
+        // );
+    }
+}
 
 export default OfficesMap;
